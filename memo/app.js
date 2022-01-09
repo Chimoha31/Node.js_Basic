890711de9e150a109e0d043f619ff480a038023f
Chimoha31 <mokochii1108@gmail.com>
2022-01-08 23:06:40 -0800

    import export

--- memo/app.js
+++ memo/app.js
@@ -2,10 +2,18 @@ console.log('app.js稼働開始');
 
 // require()関数→外部ファイルを呼び出すための関数
 const fs = require("fs");
+const os = require("os");
 
-// ES6
-fs.appendFile("greeting.txt", "Hello, World", function(err){
-  if(err) {
-    console.log(err);
-  }
-});
+const notes = require("./notes.js");
+let result = notes.addNote();
+console.log(result);
+
+// let user = os.userInfo();
+// // console.log(user);
+
+// // ES6
+// fs.appendFile("greeting.txt", "Hello," + user.username + "! You are" + notes.age + "years old", function(err){
+//   if(err) {
+//     console.log(err);
+//   }
+// });
---
+++ memo/notes.js
@@ -0,0 +1,7 @@
+console.log("Notes.js is active.")
+// console.log(module);
+// module.exports.age = 25;
+module.exports.addNote = function() {
+  console.log("addNote");
+  return "new note"
+};
